b80bc3df2b876b8809ee1ae8126cf77d7b13390c
sdeveloper8 <147605453+sdeveloper8@users.noreply.github.com>
2024-06-13 11:54:02 +0100

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,17 @@
+const span = document.querySelectorAll(".project-image span");
+console.log(span);
+
+const openCover = ((e)=>{
+
+    e.target.style.visibility = "hidden";
+
+})
+const closeCover = ((e)=>{
+    e.target.style.backgroundColor = "rgba(0, 0, 0, 0.548)";
+})
+span.forEach((e)=>{
+    e.addEventListener("mouseenter", openCover);
+})
+span.forEach((e)=>{
+    e.addEventListener("mouseleave", closeCover);
+})
